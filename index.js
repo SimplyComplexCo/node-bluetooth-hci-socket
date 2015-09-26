d8f2e6b0028b4df428b4060f13d7d218301d1e28
Paul Ryan <paul@simplycomplex.co>
2015-09-26 17:49:17 -0600

    force native for all

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ var os = require('os');
 
 var platform = os.platform();
 
-if (process.env.BLUETOOTH_HCI_SOCKET_FORCE_USB || platform === 'win32') {
-  module.exports = require('./lib/usb.js');
-} else if (platform === 'linux') {
+if (process.env.BLUETOOTH_HCI_SOCKET_FORCE_USB || platform === 'win32' || platform === 'linux') {
   module.exports = require('./lib/native');
 } else {
   throw new Error('Unsupported platform');
